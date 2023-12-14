55d70e6ada4f0758000405fd79b420ff415b9650
a.schadnev, jn <a.schadnev@diagnocat.com>
2023-12-15 03:02:55 +0400

    update

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,7 +1,9 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 
 import { BuildOptions } from '../types/types';
+
+import { buildCssLoader } from './loaders/buildCssLoader';
+import { buildSvgLoader } from './loaders/buildSvgLoader';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   const { isDev } = options;
@@ -38,34 +40,8 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     },
   };
 
-  return [
-    fileLoader,
-    {
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    },
-    babelLoader,
-    typeScriptLoader,
-    {
-      test: /\.s[ac]ss$/i,
-      use: [
-        // Creates `style` nodes from JS strings
-        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-        // Translates CSS into CommonJS
-        {
-          loader: 'css-loader',
-          options: {
-            modules: {
-              auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-              localIdentName: isDev
-                ? '[name]__[local]--[hash:base64:8]'
-                : '[hash:base64:8]',
-            },
-          },
-        },
-        // Compiles Sass to CSS
-        'sass-loader',
-      ],
-    },
-  ];
+  const svgLoader = buildSvgLoader();
+  const cssLoader = buildCssLoader(isDev);
+
+  return [fileLoader, svgLoader, babelLoader, typeScriptLoader, cssLoader];
 }
