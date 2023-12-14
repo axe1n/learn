71d25a207f5b12357dc5a11e09e84bc09204374a
a.schadnev, jn <a.schadnev@diagnocat.com>
2023-12-15 02:59:30 +0400

    update

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,9 +1,7 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 
 import { BuildOptions } from '../types/types';
-
-import { buildCssLoader } from './loaders/buildCssLoader';
-import { buildSvgLoader } from './loaders/buildSvgLoader';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   const { isDev } = options;
@@ -42,9 +40,32 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 
   return [
     fileLoader,
-    buildSvgLoader(),
+    {
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    },
     babelLoader,
     typeScriptLoader,
-    buildCssLoader(isDev),
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+        // Translates CSS into CommonJS
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+              localIdentName: isDev
+                ? '[name]__[local]--[hash:base64:8]'
+                : '[hash:base64:8]',
+            },
+          },
+        },
+        // Compiles Sass to CSS
+        'sass-loader',
+      ],
+    },
   ];
 }
